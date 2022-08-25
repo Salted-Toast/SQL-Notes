10e8cac42bc8025813dee104cfdfc1f6b8a0232f
Salted-Toast <104316350+Salted-Toast@users.noreply.github.com>
2022-08-25 02:05:53 +0100

    Create 01_Comments.sql

---
+++ Content/01_Comments.sql
@@ -0,0 +1,20 @@
+-- This is hwo to single line comment
+
+/*
+This is how to multi line comment
+*/
+
+/*==================File=Structure==================*\
+
+- Comments
+- Simple Querie Statements Using SELECT & FROM
+- Arithmatic to Simple Statements
+- How to Set Aliases
+- How to remove duplicated results via DISTINCT
+- Use WHERE
+- Go over comparitive operator and normal arythmatic ones
+- AND & OR (AND being eval First)
+- IN operator
+-  
+
+\*==================================================*/
