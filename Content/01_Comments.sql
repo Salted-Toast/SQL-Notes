d5b039b997bc7c717b0db01c74f98916a4912b94
Salted-Toast <104316350+Salted-Toast@users.noreply.github.com>
2022-08-27 23:33:57 +0100

    Update 01_Comments.sql

--- Content/01_Comments.sql
+++ Content/01_Comments.sql
@@ -108,6 +108,19 @@ FROM shippers s
 CROSS JOIN products p
 ORDER BY shipper_id
 
-- Unions and 
+- Unions and how you can group data from two queries (data can be from multiple tables)
+SELECT
+	order_id,
+    order_date,
+    'Active' AS status
+FROM orders
+WHERE order_date >= '2019-01-01'
+UNION
+SELECT
+	order_id,
+    order_date,
+    'Archived' AS status
+FROM orders
+WHERE order_date < '2019-01-01'
 
 \*==================================================*/
