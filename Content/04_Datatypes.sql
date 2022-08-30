6448975a5303825ae32e47ce58372c5afabaabf1
Salted-Toast <104316350+Salted-Toast@users.noreply.github.com>
2022-08-30 01:11:32 +0100

    .

--- Content/04_Datatypes.sql
+++ Content/04_Datatypes.sql
@@ -1,5 +1,11 @@
-/*----------------Queries----------------*\
+/*---------------Datatypes---------------*\
 
-Datatypes 
+-- Datatypes --
+
+INT(length)         Integer, whole number
+CHAR(length)        Character, if len=8 and input it hello stored as hello000
+VARCHAR(length)     VariableCharacter, if len=50 and input hello stored as hello
+DATE                Date, Values are stored as YYYY-MM-DD
+(There are more just cba)
 
 \*---------------------------------------*/
---
+++ Content/05_Aliases.sql
@@ -0,0 +1,17 @@
+/*---------------Aliases---------------*\
+
+Aliases are set after a table is mentioned
+in a SQL querey and make code look nicer 
+and easier for people to read and you to
+write.
+
+You can use this before and after this
+alias has been set like in the SELECT
+part of a querey
+
+-- SYNTAX --
+
+SELECT columns
+FROM table alias
+
+\*---------------------------------------*/
