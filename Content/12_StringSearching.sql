f829eb7a26ea4e54615a3339f72015b8750692b1
Salted-Toast <104316350+Salted-Toast@users.noreply.github.com>
2022-08-31 01:31:28 +0100

    .

--- Content/12_StringSearching.sql
+++ Content/12_StringSearching.sql
@@ -5,13 +5,35 @@ LIKE and REGEXP, these both have their own
 symbols that allow you to refine/open up
 your query
 
+-- LIKE --
+
+You can use the % symbol to say any
+characters before or after these letters,
+dependent on where you place it. And, it
+has the _ symbol which represents a singular
+char.
+
+REGEXP, you can use the ^ symbol to show
+the start of a string, the $ represents the
+end of a string, the | 
+
 -- SYNTAX --
 
 SELECT columns
 FROM table
-WHERE column IN (contents to search for)
+WHERE column LIKE 'content for search'
+
+-- SYNTAX --
+
+SELECT columns
+FROM table
+WHERE column REGEXP 'content for search'
 
 \*---------------------------------------*/
 
 USE sql_store;
+
+-- LIKE --
+
+
 
