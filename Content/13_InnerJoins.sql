b288958bed23f48cab7b860701b31811e9721549
Salted-Toast <104316350+Salted-Toast@users.noreply.github.com>
2022-08-31 02:42:50 +0100

    .

---
+++ Content/13_InnerJoins.sql
@@ -0,0 +1,14 @@
+/*--------------Inner-Joins--------------*\
+
+a
+
+-- SYNTAX --
+
+SELECT columns
+FROM table2
+INNER JOIN table2
+
+NOTE - 'INNER' is optional, if not specifyed
+        MySQL will asume its an inner join.
+
+\*---------------------------------------*/
---
+++ Content/14_OuterJoins.sql
@@ -0,0 +1,15 @@
+/*--------------Outer-Joins--------------*\
+
+a
+
+-- SYNTAX --
+
+SELECT columns
+FROM table2
+LEFT/RIGH OUTER JOIN table2
+
+NOTE - 'OUTER' is optional, if not specifyed
+        MySQL will asume its an OUTER join
+        if you have put either RIGHT/LEFT.
+
+\*---------------------------------------*/
